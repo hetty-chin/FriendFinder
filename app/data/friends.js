46175122f1e08cac73b453b90fad8e4bf90acbf3
Swiftlet <hettybootcamp@gmail.com>
2019-06-02 23:28:56 -0700

    adding more mock friends, reset the values after a survey is submitted, updated github link

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,8 +6,8 @@
 
 var friendArray = [
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Retsuko",
+        "photo": "https://s3.amazonaws.com/factmag-images/wp-content/uploads/2017/01/Aggretsuko-01-06-17-616x440.jpg",
         "scores": [
             "5",
             "1",
@@ -20,7 +20,39 @@ var friendArray = [
             "4",
             "1"
         ]
-    }
+    },
+    {
+        "name": "Haida",
+        "photo": "https://i.redd.it/yp9cfi2lmu721.jpg",
+        "scores": [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        "name": "Ton",
+        "photo": "https://cdn.shopify.com/s/files/1/0463/0789/products/1_04026c77-8ae6-4c4c-b2e9-93b97c75eecc_grande.png?v=1525148767",
+        "scores": [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
+    }    
 ];
 
 // Note how we export the array. This makes it accessible to other files using require.
